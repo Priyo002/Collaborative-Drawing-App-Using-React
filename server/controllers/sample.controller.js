df0b95efc8f3d6bbefe5ee634a05ec36a0248e4a
Priyabrata Das <priyobapi@gmail.com>
2025-04-02 21:36:06 +0530

    Fixing deployment bugs

--- server/controllers/sample.controller.js
+++
@@ -1,9 +0,0 @@
-
-
-const sampleController = (req,res) => {
-    res.status(200).json({
-        message: 'Sample Controller'
-    });
-}
-
-export {sampleController};
--- server/routes/sample.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sampleController } from '../controllers/sample.controller.js';
-
-
-const router = express.Router();
-
-router.get('/sample',sampleController);
-
-export default router;
